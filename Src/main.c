a7d8c45af7a431f34921c8e5a494aa79257ff0c9
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-12-14 04:59:35 +0100

    starting to render things

--- Src/main.c
+++ Src/main.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    SDL_Window* window = SDL_CreateWindow("PacSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    SDL_Window* pwindow = SDL_CreateWindow("PacSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
 
     bool running = true;
     while (running)
@@ -12,15 +12,19 @@ int main(void)
         SDL_Event e;
         while (SDL_PollEvent(&e) != 0)
         {
-            if (e.type == SDL_QUIT)
+            if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE))
             {
                 running = false;
                 break;
             }
+
+            SDL_Surface* psurface = SDL_GetWindowSurface(pwindow);
+            SDL_FillRect(psurface, NULL, 0xFF0000);
+            SDL_UpdateWindowSurface(pwindow);
         }
     }
 
     // clean up resources before exiting.
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(pwindow);
     SDL_Quit();
 }
